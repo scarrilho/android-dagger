ccb1446e672792824bfe9f7446eb0bf41f4f52d5
Carrilho Sergio ( SHES Sergio.Carrilho ) <Sergio.Carrilho@sony.com>
2020-02-27 17:08:15 +0900

    Finished section 10

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,12 +3,15 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
     // Factory to create instances of the AppComponent
     @Component.Factory
@@ -18,8 +21,11 @@ interface AppComponent {
 
     }
 
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
+
     // Classes that can be injected by this Component
-    fun inject(activity: RegistrationActivity)
     fun inject(activity: MainActivity)
+
 
 }
---
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
@@ -0,0 +1,19 @@
+package com.example.android.dagger.registration
+
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import dagger.Subcomponent
+
+@Subcomponent
+interface RegistrationComponent {
+    // Factory to create instances of RegistrationComponent
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): RegistrationComponent
+    }
+
+    // Definition of a Dagger subcomponent
+    fun inject(activity: RegistrationActivity)
+    fun inject(fragment: TermsAndConditionsFragment)
+    fun inject(fragment: EnterDetailsFragment)
+}
